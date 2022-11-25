b88f47e746014115ab593255b60031d5ae271b14
sriram2k4 <sriramthehacker01@gmail.com>
2022-11-25 21:48:41 +0530

    First Commit

---
+++ Data_processing.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_csv("Data.csv")
+x = data.iloc[:,:3].values
+y = data.iloc[:,-1].values
+
+# Missing Values
+imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
+imputer.fit(x[:,1:3])
+x[:, 1:3] = imputer.transform(x[:,1:3])
+
+#Encoding The labels
+label = LabelEncoder()
+y = label.fit_transform(y)
+
+print(x)
+print(y)
